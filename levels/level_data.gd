bea3aad9a7c5ef69a249c76ce809636516ccd526
Zshandi Krahn <ZshandiK@gmail.com>
2026-02-04 22:28:55 -0800

    Add resource display name function to level data
    This allows easily seeing which scene is selected without even needing to expand the level data
    
    Also add the newly created levels

--- levels/level_data.gd
+++ levels/level_data.gd
@@ -1,5 +1,10 @@
+@tool
 extends Resource
 class_name LevelData
 
 @export
-var scene: PackedScene
+var scene: PackedScene:
+	set(value):
+		scene = value
+		if scene != null:
+			resource_name = scene.resource_path.get_file().get_basename()
