c27ca9c780c2a56fd0365c37a958deee89d5d971
Zshandi Krahn <ZshandiK@gmail.com>
2026-04-18 04:27:52 -0700

    Add pause menu

--- reset_controller.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("reset_level"):
-		LevelManager.reload_level()
-	if Input.is_action_just_pressed("pause"):
-		LevelManager.load_main_menu()
